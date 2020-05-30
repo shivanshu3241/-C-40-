aa692bb45517b5100e2cf0d9b8932a8d79f657ee
shivanshu3241 <64346010+shivanshu3241@users.noreply.github.com>
2020-05-30 19:38:55 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -15,7 +15,7 @@ class Form {
   }
 
   display(){
-    this.title.html("Car Racing Game");
+    this.title.html("Hurdle Game");
     this.title.position(displayWidth/2 - 50, 0);
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
